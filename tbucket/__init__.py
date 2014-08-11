e97c83d7f97c14c3cad8988ee7df8612e6027ef7
Fabien MARTY <fabien.marty@gmail.com>
2014-08-11 17:23:37 +0200

    missing header

--- tbucket/__init__.py
+++ tbucket/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# This file is part of tbucket daemon released under the MIT license.
+# See the LICENSE file for more information.
 
 version_info = (0, 0, '1')
 __version__ = ".".join([str(x) for x in version_info])
